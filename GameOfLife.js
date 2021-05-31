19bedab0fde97a49ad8fa2ea34430433650e7ce1
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2021-05-31 15:49:27 -0400

    refac: optimize - diffs only

--- GameOfLife.js
+++ GameOfLife.js
@@ -1,43 +1,115 @@
 class GameOfLife {
-  constructor(rowCount, colCount, density) {
-    this.board = new Array(rowCount)
-      .fill([])
-      .map(() =>
-        new Array(colCount).fill(0).map(() => (Math.random() < density ? 1 : 0))
+  constructor(rowCount, colCount, density, CTX) {
+    this.rowCount = rowCount;
+    this.colCount = colCount;
+    this.density = density / 100;
+    this.CTX = CTX;
+
+    this.CELL_SIZE = 16;
+    this.FULL_SIZE = this.CELL_SIZE + 1; // border
+
+    // set up canvas
+    const height = 1 + this.rowCount * this.FULL_SIZE;
+    const width = 1 + this.colCount * this.FULL_SIZE;
+
+    this.CTX.canvas.height = height;
+    this.CTX.canvas.width = width;
+
+    // draw grid
+    this.CTX.strokeStyle = "lightGray";
+    this.CTX.beginPath();
+    this.CTX.moveTo(0, 0);
+    for (let i = 0; i <= this.rowCount; i++) {
+      this.CTX.lineTo(width, i * this.FULL_SIZE);
+      this.CTX.moveTo(0, (i + 1) * this.FULL_SIZE);
+    }
+    this.CTX.moveTo(0, 0);
+    for (let i = 0; i <= this.colCount; i++) {
+      this.CTX.lineTo(i * this.FULL_SIZE, height);
+      this.CTX.moveTo((i + 1) * this.FULL_SIZE, 0);
+    }
+    this.CTX.stroke();
+    this.CTX.fillStyle = "darkBlue";
+
+    // create empty board
+    this.board = (() => {
+      const board = [];
+      for (let rowI = 0; rowI < this.rowCount; rowI++) {
+        const row = [];
+        for (let colI = 0; colI < this.colCount; colI++) {
+          row.push(0);
+        }
+        board.push(row);
+      }
+      return board;
+    })();
+
+    // seed diff
+    this._applyDiff(() => {
+      const diff = [];
+      for (let rowI = 0; rowI < this.rowCount; rowI++) {
+        for (let colI = 0; colI < this.colCount; colI++) {
+          if (Math.random() < this.density) diff.push([rowI, colI, 1]);
+        }
+      }
+      return diff;
+    });
+  }
+
+  _applyDiff(calculateDiff) {
+    const diff = calculateDiff();
+
+    for (let i = 0; i < diff.length; i++) {
+      const [rowI, colI, newVal] = diff[i];
+
+      this.board[rowI][colI] = newVal;
+
+      this.CTX[newVal ? "fillRect" : "clearRect"](
+        1 + colI * this.FULL_SIZE,
+        1 + rowI * this.FULL_SIZE,
+        this.CELL_SIZE,
+        this.CELL_SIZE
       );
+    }
   }
 
   toggle(row, col) {
-    this.board[row][col] = this.board[row][col] === 0 ? 1 : 0;
+    this._applyDiff(() => [[row, col, this.board[row][col] === 0 ? 1 : 0]]);
   }
 
   tick() {
-    this.board = this.board.map((row, rowI) =>
-      row.map((cell, colI) => {
-        const liveNeighborCount =
-          this.board[(rowI + this.rowCount - 1) % this.rowCount][
-            (colI + this.colCount - 1) % this.colCount
-          ] +
-          this.board[(rowI + this.rowCount - 1) % this.rowCount][colI] +
-          this.board[(rowI + this.rowCount - 1) % this.rowCount][
-            (colI + 1) % this.colCount
-          ] +
-          this.board[rowI][(colI + 1) % this.colCount] +
-          this.board[(rowI + 1) % this.rowCount][(colI + 1) % this.colCount] +
-          this.board[(rowI + 1) % this.rowCount][colI] +
-          this.board[(rowI + 1) % this.rowCount][
-            (colI + this.colCount - 1) % this.colCount
-          ] +
-          this.board[rowI][(colI + this.colCount - 1) % this.colCount];
-        // from https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-        // Any live cell with two or three live neighbours survives.
-        if (cell === 1 && liveNeighborCount >= 2 && liveNeighborCount <= 3)
-          return 1;
-        // Any dead cell with three live neighbours becomes a live cell.
-        if (cell === 0 && liveNeighborCount === 3) return 1;
-        // All other live cells die in the next generation. Similarly, all other dead cells stay dead.
-        return 0;
-      })
-    );
+    this._applyDiff(() => {
+      const diff = [];
+      for (let rowI = 0; rowI < this.rowCount; rowI++) {
+        for (let colI = 0; colI < this.colCount; colI++) {
+          const cell = this.board[rowI][colI];
+          const liveNeighborCount =
+            this.board[(rowI + this.rowCount - 1) % this.rowCount][
+              (colI + this.colCount - 1) % this.colCount
+            ] +
+            this.board[(rowI + this.rowCount - 1) % this.rowCount][colI] +
+            this.board[(rowI + this.rowCount - 1) % this.rowCount][
+              (colI + 1) % this.colCount
+            ] +
+            this.board[rowI][(colI + 1) % this.colCount] +
+            this.board[(rowI + 1) % this.rowCount][(colI + 1) % this.colCount] +
+            this.board[(rowI + 1) % this.rowCount][colI] +
+            this.board[(rowI + 1) % this.rowCount][
+              (colI + this.colCount - 1) % this.colCount
+            ] +
+            this.board[rowI][(colI + this.colCount - 1) % this.colCount];
+
+          if (cell === 1 && (liveNeighborCount < 2 || liveNeighborCount > 3)) {
+            // if cell is on and has less than 2 or more than 3 on neighbors, it turns off
+            diff.push([rowI, colI, 0]);
+          } else if (cell === 0 && liveNeighborCount === 3) {
+            // if a cell is off and has 3 on neighbors, it turns on
+            diff.push([rowI, colI, 1]);
+          }
+          // otherwise, on cells remain on and off cells remain off
+        }
+      }
+      return diff;
+    });
   }
 }
--- main.js
+++ main.js
@@ -8,61 +8,18 @@ window.onload = () => {
   const INPUT_DENSITY = document.getElementById("density");
   const INPUT_FRAMES = document.getElementById("frames");
 
-  const CTX = BOARD.getContext("2d");
-  const BORDER_SIZE = 1;
-  const CELL_SIZE = 16;
-  const FULL_SIZE = CELL_SIZE + BORDER_SIZE;
-
-  const paintCell = (rowI, colI) => {
-    CTX[GOL.board[rowI][colI] ? "fillRect" : "clearRect"](
-      1 + colI * FULL_SIZE,
-      1 + rowI * FULL_SIZE,
-      CELL_SIZE,
-      CELL_SIZE
-    );
-  };
-
-  const paintBoard = () => {
-    GOL.board.forEach((row, rowI) => {
-      row.forEach((_, colI) => {
-        paintCell(rowI, colI);
-      });
-    });
-  };
-
   let GOL = null;
   let RAF_ID = null;
 
   BTN_CREATE.addEventListener("click", () => {
     if (RAF_ID) BTN_PLAY_PAUSE.click();
 
-    const rowCount = Number(INPUT_ROWS.value);
-    const colCount = Number(INPUT_COLS.value);
-    const density = Number(INPUT_DENSITY.value) / 100;
-    GOL = new GameOfLife(rowCount, colCount, density);
-
-    const height = 1 + rowCount * FULL_SIZE;
-    const width = 1 + colCount * FULL_SIZE;
-
-    CTX.canvas.height = height;
-    CTX.canvas.width = width;
-
-    CTX.strokeStyle = "lightgray";
-    CTX.beginPath();
-    CTX.moveTo(0, 0);
-    for (let i = 0; i <= rowCount; i++) {
-      CTX.lineTo(width, i * FULL_SIZE);
-      CTX.moveTo(0, (i + 1) * FULL_SIZE);
-    }
-    CTX.moveTo(0, 0);
-    for (let i = 0; i <= colCount; i++) {
-      CTX.lineTo(i * FULL_SIZE, height);
-      CTX.moveTo((i + 1) * FULL_SIZE, 0);
-    }
-    CTX.stroke();
-
-    CTX.fillStyle = "darkBlue";
-    paintBoard();
+    GOL = new GameOfLife(
+      Number(INPUT_ROWS.value),
+      Number(INPUT_COLS.value),
+      Number(INPUT_DENSITY.value),
+      BOARD.getContext("2d")
+    );
   });
 
   BOARD.addEventListener("click", (evt) => {
@@ -70,18 +27,16 @@ window.onload = () => {
     const y = Math.abs(Math.floor(evt.clientY - top));
     const x = Math.abs(Math.floor(evt.clientX - left));
 
-    if (x % FULL_SIZE !== 0 && y % FULL_SIZE !== 0) {
-      const rowI = Math.floor(y / FULL_SIZE);
-      const colI = Math.floor(x / FULL_SIZE);
+    if (x % GOL.FULL_SIZE !== 0 && y % GOL.FULL_SIZE !== 0) {
+      const rowI = Math.floor(y / GOL.FULL_SIZE);
+      const colI = Math.floor(x / GOL.FULL_SIZE);
 
       GOL.toggle(rowI, colI);
-      paintCell(rowI, colI);
     }
   });
 
   BTN_STEP.addEventListener("click", () => {
     GOL.tick();
-    paintBoard();
   });
 
   BTN_PLAY_PAUSE.addEventListener("click", () => {
@@ -93,10 +48,7 @@ window.onload = () => {
       let count = 0;
 
       const animateSteps = () => {
-        if (count === 0) {
-          GOL.tick();
-          paintBoard();
-        }
+        if (count === 0) GOL.tick();
 
         count += 1;
         count %= frames;
