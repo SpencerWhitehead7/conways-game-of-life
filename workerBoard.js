395d7da53c061af8b3c7b237c8c389bdb48ccc67
Spencer Whitehead <SpencerWhitehead7@gmail.com>
2025-02-20 23:30:11 -0500

    refac: Int32Array view and manual bit shifting + loop unroll

--- workerBoard.js
+++ workerBoard.js
@@ -16,20 +16,60 @@ Comlink.expose({
       let turnOnI = 0;
       let turnOffI = 0;
 
-      for (let i = 0; i < board.length; i++) {
+      const Int32Board = new Int32Array(board.buffer);
+      for (let i = 0; i < Int32Board.length; i++) {
         // Any live cell with two or three live neighbours survives.
         // Similarly, all other dead cells stay dead.
-        const cell = board[i];
+
+        let int32Cell = Int32Board[i];
+        let cell = int32Cell & 255;
         if (cell === 30) {
           // Any dead cell with three live neighbours becomes a live cell.
-          toggleCell(i, 1);
-          turnOnIdxsPreallocated[turnOnI++] = i;
+          toggleCell(i * 4, 1);
+          turnOnIdxsPreallocated[turnOnI++] = i * 4;
         } else if ((cell & 1) === 1 && (cell < 21 || cell > 31)) {
           // All other live cells die in the next generation.
-          toggleCell(i, -1);
-          turnOffIdxsPreallocated[turnOffI++] = i;
+          toggleCell(i * 4, -1);
+          turnOffIdxsPreallocated[turnOffI++] = i * 4;
+        }
+
+        int32Cell >>>= 8;
+        cell = int32Cell & 255;
+        if (cell === 30) {
+          // Any dead cell with three live neighbours becomes a live cell.
+          toggleCell(i * 4 + 1, 1);
+          turnOnIdxsPreallocated[turnOnI++] = i * 4 + 1;
+        } else if ((cell & 1) === 1 && (cell < 21 || cell > 31)) {
+          // All other live cells die in the next generation.
+          toggleCell(i * 4 + 1, -1);
+          turnOffIdxsPreallocated[turnOffI++] = i * 4 + 1;
+        }
+
+        int32Cell >>>= 8;
+        cell = int32Cell & 255;
+        if (cell === 30) {
+          // Any dead cell with three live neighbours becomes a live cell.
+          toggleCell(i * 4 + 2, 1);
+          turnOnIdxsPreallocated[turnOnI++] = i * 4 + 2;
+        } else if ((cell & 1) === 1 && (cell < 21 || cell > 31)) {
+          // All other live cells die in the next generation.
+          toggleCell(i * 4 + 2, -1);
+          turnOffIdxsPreallocated[turnOffI++] = i * 4 + 2;
+        }
+
+        int32Cell >>>= 8;
+        cell = int32Cell & 255;
+        if (cell === 30) {
+          // Any dead cell with three live neighbours becomes a live cell.
+          toggleCell(i * 4 + 3, 1);
+          turnOnIdxsPreallocated[turnOnI++] = i * 4 + 3;
+        } else if ((cell & 1) === 1 && (cell < 21 || cell > 31)) {
+          // All other live cells die in the next generation.
+          toggleCell(i * 4 + 3, -1);
+          turnOffIdxsPreallocated[turnOffI++] = i * 4 + 3;
         }
       }
+      // I don't understand why the + values for physical index in the unit8 array seem "backward," but it's working
 
       const turnOnIdxs = turnOnIdxsPreallocated.slice(0, turnOnI);
       const turnOffIdxs = turnOffIdxsPreallocated.slice(0, turnOffI);
